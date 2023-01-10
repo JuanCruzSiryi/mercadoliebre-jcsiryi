0fa3d74bac3bcccd76388c71e33b8dde98b41abd
JuanCruzSiryi <juancruzsiryi@gmail.com>
2023-01-10 20:25:10 -0300

    add register & login

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.get('/', function(req, res){
     res.sendFile(htmlPath);
 });
 
+app.get('/register', function(req, res){
+    let htmlPath = path.resolve(__dirname, "./views/register.html");
+    res.sendFile(htmlPath);
+});
+
+app.get('/login', function(req, res){
+    let htmlPath = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(htmlPath);
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
