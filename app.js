b08aeee516109ab87644e5d469a460520aba0ce7
JuanCruzSiryi <juancruzsiryi@gmail.com>
2023-01-17 19:40:51 -0300

    change const port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 app.get('/', function(req, res){
